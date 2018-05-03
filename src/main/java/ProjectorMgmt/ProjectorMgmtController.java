3fa716d3398bcd53c8cc2074032dd979aa2e9399
Ray Zhou <zhou.ray@gmail.com>
2018-05-02 17:26:39 -0700

    adding error response for get /projector

--- src/main/java/ProjectorMgmt/ProjectorMgmtController.java
+++ src/main/java/ProjectorMgmt/ProjectorMgmtController.java
@@ -2,6 +2,8 @@ package ProjectorMgmt;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -45,8 +47,14 @@ public class ProjectorMgmtController {
     Database db;
 
     @RequestMapping(value = "/projectors", method = RequestMethod.GET)
-    public ArrayDataResponse listProjectors() {
-        return (new ArrayDataResponse(db.getEquipmentList()));
+    public ResponseEntity<ArrayDataResponse> listProjectors() {
+        ArrayList<Projector> equipmentList = db.getEquipmentList();
+        equipmentList = null;
+        if (equipmentList != null) {
+            return new ResponseEntity(new ArrayDataResponse(db.getEquipmentList()), HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 
