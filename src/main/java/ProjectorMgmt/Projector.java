44b9319c157dc5bc6753d31ae8e62899db81b17f
Ray Zhou <zhou.ray@gmail.com>
2018-05-02 17:27:58 -0700

    made object follow json convention

--- src/main/java/ProjectorMgmt/Projector.java
+++ src/main/java/ProjectorMgmt/Projector.java
@@ -1,14 +1,49 @@
 package ProjectorMgmt;
 
-public class Projector {
-    private final String name;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-    public Projector (int id) {
-        this.name = new String("P" + Integer.valueOf(id));
+class Attributes {
+    private String name;
+    
+    public Attributes(String name){
+        this.name = name;
     }
-
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     public String getName() {
         return this.name;
+    }
+}
+
+public class Projector {
+    private final int id;
+    private final Attributes attributes;
+    private final String type;
+
+    public Projector (int id) {
+        this.type = new String("projector");
+        this.id = id;
+        this.attributes = new Attributes(new String("P" + Integer.valueOf(id)));
+    }
+    
+    public Attributes getAttributes() {
+        return this.attributes;
+    }
+    
+    @JsonIgnore
+    public String getName() {
+        return this.attributes.getName();
+    }
+
+    public String getType() {
+        return this.type;
+    }
+    
+    public int getId() {
+        return this.id;
     }
 
 }
