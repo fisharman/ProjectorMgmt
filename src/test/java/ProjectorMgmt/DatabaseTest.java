1ef46b69b2ba67df8b1a122b5d153b362c90974d
Ray Zhou <zhou.ray@gmail.com>
2018-05-03 22:24:17 -0700

    adding tests for the database

--- src/test/java/ProjectorMgmt/DatabaseTest.java
+++ src/test/java/ProjectorMgmt/DatabaseTest.java
@@ -4,63 +4,83 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class DatabaseTest {
     private final int EQUIPMENT_COUNT = 10;
     private ArrayList<Projector> projectors;
-    private Map<String, BookingSlot> bookings;
-    private Map<String, ArrayList<BookingSlot>> projectorStatus;
-
+    
     @Before
     public void setUp() throws Exception {
-        bookings = new HashMap<>();
-        projectorStatus = new HashMap<>();
         projectors = new ArrayList<>(EQUIPMENT_COUNT);
 
         for (int i = 1; i <= EQUIPMENT_COUNT; i++){
             Projector projector = new Projector(i);
-            projectorStatus.putIfAbsent(projector.getName(), new ArrayList<>() );
             this.projectors.add(projector);
         }
     }
 
     @After
     public void tearDown() throws Exception {
+        projectors = null;
     }
 
     @Test
     public void getEquipmentList() {
-    }
-
-    @Test
-    public void setReservation() {
-    }
-
-    @Test
-    public void checkSchedule() {
-        String date = new String("2018-04-28");
-        String startTime = new String ("12:00");
-        String endTime = new String("12:30");
-
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
-        LocalDate parsedD = LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE);
-        LocalDateTime parsedSDT = LocalDateTime.of(parsedD, LocalTime.parse(startTime, formatter));
-        LocalDateTime parsedEDT = LocalDateTime.of(parsedD, LocalTime.parse(endTime, formatter));
-
-
-        for (int i = 0; i < 10; i++){
-        //    assertTrue(Database.checkSchedule(new BookingSlot(), projectorStatus, parsedSDT.plusMinutes(1), parsedEDT));
+        int i = 1;
+        for (Projector projector : projectors){
+            assertEquals(projector.getId(), i);
+            assertEquals(projector.getName(), new String("P" + Integer.toString(i++)));
         }
+    }
 
-        //assertFalse(Database.checkSchedule(objectNode, projectorStatus, parsedSDT.plusMinutes(1), parsedEDT));
+    @Test
+    public void addBooking() throws Exception {
+        String date = new String("2018-04-28");
+        String startT = new String ("12:00");
+        String endT = new String("13:00");
 
+        LocalDateTime parsedSDT = ConvertDateTime.convertDT(date, startT);
+        LocalDateTime parsedEDT = ConvertDateTime.convertDT(date, endT);
+
+        BookingSlot bookingSlot = new BookingSlot(parsedSDT, parsedEDT);
+        Map<String, ArrayList<BookingSlot>> mockProjectorStatus = new HashMap<>();
+        mockProjectorStatus.put(projectors.get(0).getName(), new ArrayList<BookingSlot>());
+        
+        assertTrue(Database.checkSchedule(bookingSlot, mockProjectorStatus, parsedSDT, parsedEDT));
+    }
+    
+    @Test
+    public void checkBlockingSchedule() throws Exception {
+        String date = new String("2018-04-28");
+        String startT = new String ("12:00");
+        String endT = new String("13:00");
+        
+        LocalDateTime parsedSDT = ConvertDateTime.convertDT(date, startT);
+        LocalDateTime parsedEDT = ConvertDateTime.convertDT(date, endT);
+        
+        BookingSlot bookingSlot = new BookingSlot(parsedSDT, parsedEDT);
+        Map<String, ArrayList<BookingSlot>> mockProjectorStatus = generateSameBooking(parsedSDT, parsedEDT);
+
+        assertFalse(Database.checkSchedule(bookingSlot, mockProjectorStatus, parsedSDT, parsedEDT));
+    }
+    
+    private Map<String, ArrayList<BookingSlot>> generateSameBooking (LocalDateTime startTime, LocalDateTime endTime) {
+        Map<String, ArrayList<BookingSlot>> mockProjectorStatus = new HashMap<>();
+        
+        for (int i = 0; i < EQUIPMENT_COUNT; i++){
+            ArrayList<BookingSlot> arrayList = new ArrayList<>();
+            BookingSlot bookingSlot = new BookingSlot(startTime, endTime);
+            arrayList.add(bookingSlot);
+            mockProjectorStatus.putIfAbsent(projectors.get(i).getName(), arrayList);    
+        }
+        return mockProjectorStatus;
     }
 }
